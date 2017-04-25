8107b71d9f1d96c9f12ecebfd8c4b1dfd11fbf83
Anwesh43 <anweshthecool0@gmail.com>
2017-04-25 17:07:21 +0530

    Adding Box and rotating the text

--- index.vr.js
+++ index.vr.js
@@ -6,24 +6,27 @@ import {
   Pano,
   Text,
   View,
+  Box
 } from 'react-vr';
 
 export default class ReactVRFirst extends React.Component {
     render() {
     const textStrings = ["Hello","Strike","Scar"]
-    const textViews = textStrings.map((text)=>(<View style={{margin:0.1,height:0.3,backgroundColor:'#009688'}}><Text style={{font:5,color:'white',textAlign:'center'}}>{text}</Text></View>))
+    const textViews = textStrings.map((text)=>(<View style={{margin:0.1,height:0.3,backgroundColor:'#009688'}}><Text style={{fontSize:0.2,color:'white',textAlign:'center'}}>{text}</Text></View>))
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset('chess-world.jpg')}>
         <View style={{
           flex:1,
           width:2,
           flexDirection:'column',
           alignItems:'stretch',
-          transform:[{translate:[-1,1,-4]}]
+          transform:[{translate:[-1,-1,-5]},{rotateX:90}]
         }}>
         {textViews}
         </View>
+        <Box dimWidth={1} dimHeight={1} dimDepth={1} style={{color:'#7E57C2',transform:[{translate:[-1,2,-5]}]}}/>
+      </Pano>
       </View>
     );
   }
